e176bc60ce8a768d0020bf3d7d401431c043f3e2
zzxx-husky <zzxx.is.me@gmail.com>
2020-01-21 20:27:20 +0800

    Build Static Library

--- build_g.sh
+++ build_g.sh
@@ -19,5 +19,12 @@ CFLAG+=" -fno-builtin-valloc"
 CFLAG+=" -fno-builtin-memalign"
 CFLAG+=" -fno-builtin-pvalloc"
 CFLAG+=" -fno-builtin-malloc_usable_size"
-g++ -DKANAMESHIKI_HEAP_SPECIALIZATION=1 src/KanameShiki.cpp ${CFLAG} -o KanameShiki1_g.so
-g++ -DKANAMESHIKI_HEAP_SPECIALIZATION=0 src/KanameShiki.cpp ${CFLAG} -o KanameShiki0_g.so
+
+g++ -DKANAMESHIKI_HEAP_SPECIALIZATION=1 src/KanameShiki.cpp ${CFLAG} -o libKanameShiki1_g.so
+g++ -DKANAMESHIKI_HEAP_SPECIALIZATION=0 src/KanameShiki.cpp ${CFLAG} -o libKanameShiki0_g.so
+
+g++ -c -DKANAMESHIKI_HEAP_SPECIALIZATION=1 src/KanameShiki.cpp ${CFLAG} -o KanameShiki1_g.o
+g++ -c -DKANAMESHIKI_HEAP_SPECIALIZATION=0 src/KanameShiki.cpp ${CFLAG} -o KanameShiki0_g.o
+
+ar rcs libKanameShiki1_g.a KanameShiki1_g.o
+ar rcs libKanameShiki0_g.a KanameShiki0_g.o
